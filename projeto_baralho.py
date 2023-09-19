d06996212725e016b01a4caec6e238b22f4b3075
Nicollas Jahel <nicollasjahel@gmail.com>
2023-09-18 22:57:21 -0300

    Projeto baralho

---
+++ projeto_baralho.py
@@ -0,0 +1,28 @@
+import random
+
+NAIPES = '♥ ♦ ♣ ♠'.split()
+CARTAS = '2 3 4 5 6 7 8 9 10 J Q K A'.split()
+
+def criar_baralho(aleatorio=False):
+    '''Cria um baralho de 52 cartas'''
+    baralho = [(n, c) for c in CARTAS for n in NAIPES]
+    if aleatorio:
+        random.shuffle(baralho)  # serve para embaralhar uma lista
+    return baralho
+
+def distribuir_cartas(baralho):
+    '''Gerencia a mão de cartas de acordo com o baralho gerado'''
+    return baralho[0::4], baralho[1::4], baralho[2::4], baralho[3::4]
+
+def jogar():
+    """Inicia um jogo de cartas para 4 jogadores"""
+    cartas = criar_baralho(aleatorio=True)
+    jogadores = 'P1 P2 P3 P4'.split()
+    maos = {j: m for j, m in zip(jogadores, distribuir_cartas(cartas))}
+
+    for jogador, cartas in maos.items():
+        carta = " ".join(f"{j}{c}" for (j,c) in cartas)
+        print(f'{jogador}: {carta}')
+
+if __name__ == '__main__':
+    jogar()
